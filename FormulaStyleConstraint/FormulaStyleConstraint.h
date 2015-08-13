95aee921e1cc9880d94a95777467a238e7dc25a0
fhisa <hisa@fobj.com>
2015-08-13 17:17:11 +0900

    Initial Commit

---
+++ FormulaStyleConstraint/FormulaStyleConstraint.h
@@ -0,0 +1,19 @@
+//
+//  FormulaStyleConstraint.h
+//  FormulaStyleConstraint
+//
+//  Created by fhisa on 2015/08/13.
+//  Copyright (c) 2015年 Hisakuni Fujimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormulaStyleConstraint.
+FOUNDATION_EXPORT double FormulaStyleConstraintVersionNumber;
+
+//! Project version string for FormulaStyleConstraint.
+FOUNDATION_EXPORT const unsigned char FormulaStyleConstraintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormulaStyleConstraint/PublicHeader.h>
+
+
